338b8d88a029239b5cdb361f95ed92eebd4df06f
Viraat Kumar <viraat.kumar7@gmail.com>
2026-07-08 03:30:05 +0530

    adding chat service and admin controller but admin controller doesnt have any code yet

--- src/main/java/com/swapper/monolith/controller/AdminController.java
+++ src/main/java/com/swapper/monolith/controller/AdminController.java
@@ -1,11 +1,25 @@
 package com.swapper.monolith.controller;
 
+import com.swapper.monolith.ChatService.dtos.MessageDto;
+import com.swapper.monolith.service.AdminService;
 import com.swapper.monolith.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
 
+    private final AdminService adminService;
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+    @PostMapping("/trade/delete/{order_id}")
+    public ResponseEntity<MessageDto> deleteMessage(@RequestParam(name="order_id") String orderId){
+        return ResponseEntity.ok(null);
+    }
 }
---
+++ src/main/java/com/swapper/monolith/service/AdminService.java
@@ -0,0 +1,11 @@
+package com.swapper.monolith.service;
+
+import com.swapper.monolith.TradeService.service.TradeService;
+import org.hibernate.query.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+    TradeService tradeService;
+}
